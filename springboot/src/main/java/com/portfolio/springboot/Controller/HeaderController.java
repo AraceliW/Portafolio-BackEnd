88c5ad6a06a898069050ca1f565c656c5751bec3
AraceliW <araceli.lopez160101@gmail.com>
2023-03-26 22:47:13 -0300

    Ultimo componente Header funcionando

--- springboot/src/main/java/com/portfolio/springboot/Controller/HeaderController.java
+++ springboot/src/main/java/com/portfolio/springboot/Controller/HeaderController.java
@@ -1,22 +1,24 @@
 package com.portfolio.springboot.Controller;
 
-
 import com.portfolio.springboot.Dto.dtoHeader;
 import com.portfolio.springboot.Entity.Header;
 import com.portfolio.springboot.Security.Controller.Mensaje;
 import com.portfolio.springboot.Service.HeaderService;
-import java.util.List;
+import java.util.Optional;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+/*import org.springframework.web.bind.annotation.DeleteMapping;*/
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+/*import org.springframework.web.bind.annotation.PostMapping;*/
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 @RequestMapping("/header")
@@ -25,12 +27,34 @@ public class HeaderController {
     @Autowired
     HeaderService sHeader;
 
-    @GetMapping("/lista")
-    public ResponseEntity<List<Header>> list() {
-        List<Header> list = sHeader.list();
-        return new ResponseEntity(list, HttpStatus.OK);
+    
+   @GetMapping("/traer-header")
+    public ResponseEntity<Header> findHeader() {
+    Optional<Header> header = sHeader.getOne(1);
+    if (header.isPresent()) {
+        return new ResponseEntity<>(header.get(), HttpStatus.OK);
+    } else {
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
+}
 
+   
+   
+    /*@PostMapping("/create")
+     public ResponseEntity<?> create(@RequestBody dtoHeader dtohea) {
+        if (StringUtils.isBlank(dtohea.getBannerPersona())) {
+            return new ResponseEntity(new Mensaje("Debes ingresar el banner"), HttpStatus.BAD_REQUEST);
+        }
+        if (sHeader.existByBannerPersona(dtohea.getBannerPersona())) {
+            return new ResponseEntity(new Mensaje("Ya existe ese banner"), HttpStatus.BAD_REQUEST);
+        }
+        Header header = new Header(
+                dtohea.getBannerPersona());
+              
+        sHeader.save(header);
+        return new ResponseEntity(new Mensaje("Se agregó correctamente!"), HttpStatus.OK);
+    }*/
+    
     
     
     @PutMapping("/update/{id}")
@@ -44,23 +68,34 @@ public class HeaderController {
     if (StringUtils.isBlank(dtohea.getBannerPersona())) {
         return new ResponseEntity(new Mensaje("Debes ingresar el banner"), HttpStatus.BAD_REQUEST);
     }
+    
 
     Header header = sHeader.getOne(id).get();
     header.setBannerPersona(dtohea.getBannerPersona());
 
-    sHeader.update(header); // Cambiar esta línea
+    sHeader.save(header); // Cambiar esta línea
 
     return new ResponseEntity(new Mensaje("Se actualizó el banner"), HttpStatus.OK);
     }
     
     
     
+    /*@DeleteMapping("/delete/{id}")
+    public ResponseEntity<?> delete(@PathVariable("id") int id) {
+        if (!sHeader.existById(id)) {
+            return new ResponseEntity(new Mensaje("No existe ese ID"), HttpStatus.BAD_REQUEST);
+            }
+        sHeader.delete(id);
+        
+    return new ResponseEntity(new Mensaje("Se eliminó el header"), HttpStatus.OK);
+    }*/
+
     
 
     @GetMapping("/detail/{id}")
     public ResponseEntity<Header> getById(@PathVariable("id") int id) {
         if (!sHeader.existById(id)) {
-            return new ResponseEntity(new Mensaje("no existe el id"), HttpStatus.NOT_FOUND);
+            return new ResponseEntity(new Mensaje("No existe el id"), HttpStatus.NOT_FOUND);
         }
 
         Header header = sHeader.getOne(id).get();
--- springboot/src/main/java/com/portfolio/springboot/Service/HeaderService.java
+++ springboot/src/main/java/com/portfolio/springboot/Service/HeaderService.java
@@ -4,9 +4,7 @@ import com.portfolio.springboot.Entity.Header;
 import com.portfolio.springboot.Repository.HeaderRepository;
 import jakarta.transaction.Transactional;
 import java.util.List;
-import java.util.NoSuchElementException;
 import java.util.Optional;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,32 +18,25 @@ public class HeaderService {
         return rHeader.findAll();
     }
     
+    
     public Optional<Header> getOne(int id) {
         return rHeader.findById(id);
     }
+    
     
     public Optional<Header> getByBannerPersona(String bannerPersona) {
         return rHeader.findByBannerPersona(bannerPersona);
     }
     
     
-    //SOLO PERMITE LA EDICION, NO PERMITE AGREGAR O ELIMINAR
-     public void update(Header updatedHeader) {
-        Optional<Header> header = rHeader.findById(updatedHeader.getId());
-        if (header.isPresent()) {
-            Header existingHeader = header.get();
-            if (!StringUtils.isBlank(updatedHeader.getBannerPersona())) {
-                existingHeader.setBannerPersona(updatedHeader.getBannerPersona());
-                // actualizar otros campos si es necesario
-                rHeader.save(existingHeader);
-            } else {
-                throw new IllegalArgumentException("El campo de la portada no puede estar vacío");
-            }
-        } else {
-            throw new NoSuchElementException("El registro que se desea actualizar no existe");
-        }
+     public void save(Header bannerPersona) {
+        rHeader.save(bannerPersona);
     }
-    
+     
+     
+    public void delete(int id) {
+       rHeader.deleteById(id);
+    }
    
     
     
