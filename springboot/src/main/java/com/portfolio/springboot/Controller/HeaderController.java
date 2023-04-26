4ca6a2345624b958f256ebe4c27b5dff964bd7a2
AraceliW <araceli.lopez160101@gmail.com>
2023-04-26 00:01:16 -0300

    Cambio de dominio

--- springboot/src/main/java/com/portfolio/springboot/Controller/HeaderController.java
+++ springboot/src/main/java/com/portfolio/springboot/Controller/HeaderController.java
@@ -22,7 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/header")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://portfolio-frontend-ary.web.app")
 public class HeaderController {
     @Autowired
     HeaderService sHeader;
